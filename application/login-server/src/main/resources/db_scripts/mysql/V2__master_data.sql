5ae1a2424d68e82c2f908703a765deea31ed5c83
r0p06np <rabindra.kumar.patra@walmart.com>
2021-12-30 12:31:26 +0530

    flyway master data for user admin tables

---
+++ application/login-server/src/main/resources/db_scripts/mysql/V2__master_data.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS TBL_LOGIN_USER
+(
+UUID VARCHAR(100) PRIMARY KEY,
+USERNAME VARCHAR(256) UNIQUE NOT NULL,
+PASSWORD VARCHAR(256) NOT NULL,
+STATUS VARCHAR(40) NOT NULL
+);
+
+INSERT INTO TBL_LOGIN_USER VALUES
+(
+'user-uuid1','x12345','password@123!','DRAFT'
+),
+(
+'user-uuid2','x12346','password@123!','DRAFT'
+),
+(
+'user-uuid3','x12347','password@123!','DRAFT'
+),
+(
+ 'user-uuid4','x12348','password@123!','DRAFT'
+);
+
+INSERT INTO TBL_LOGIN_ROLE VALUES
+('role-uuid1','ADMIN');
+
+INSERT INTO TBL_LOGIN_USER_ROLE_MAPPING VALUES
+('user-role-uuid1','user-uuid1','role-uuid1'),
+('user-role-uuid2','user-uuid2','role-uuid1'),
+('user-role-uuid3','user-uuid3','role-uuid1'),
+('user-role-uuid4','user-uuid4','role-uuid1');
+
