0a4341fc451b678e94fd4712e090549b5f1ff5da
saroj-kumar-barik <sarojkumarbarik1143@gmail.com>
2022-01-09 16:53:40 +0530

    getUserByUserName implementatoion

--- framework/login-security/src/main/java/io/login/security/dao/UserDaoRepository.java
+++ framework/login-security/src/main/java/io/login/security/dao/UserDaoRepository.java
@@ -1,18 +1,24 @@
 package io.login.security.dao;
 
+import io.login.client.models.UserStatus;
 import io.login.security.models.LoginUser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Repository
 public class UserDaoRepository implements IUserRepository {
 
     private static Logger LOGGER = LoggerFactory.getLogger(UserDaoRepository.class);
+
+    private static final String SELECT_USER_BY_USERNAME = "SELECT * FROM TBL_LOGIN_USER WHERE USERNAME=?";
 
     private JdbcTemplate jdbcTemplate;
 
@@ -32,7 +38,8 @@ public class UserDaoRepository implements IUserRepository {
     }
     @Override
     public LoginUser getUserByUsername(String username) {
-        return null;
+        Object[] objects = new Object[]{username};
+        return jdbcTemplate.queryForObject(SELECT_USER_BY_USERNAME, new UserRowMapper(), objects);
     }
 
     @Override
@@ -55,4 +62,22 @@ public class UserDaoRepository implements IUserRepository {
         return false;
     }
 
+
+    private static class UserRowMapper implements RowMapper<LoginUser>{
+
+        @Override
+        public LoginUser mapRow(ResultSet rs, int rowNum) throws SQLException {
+            String uuid = rs.getString("UUID");
+            String username = rs.getString("USERNAME");
+            String password = rs.getString("PASSWORD");
+            String  status = rs.getString("STATUS");
+
+            LoginUser loginUser = new LoginUser();
+            loginUser.setUuid(uuid);
+            loginUser.setUsername(username);
+            loginUser.setPassword(password);
+            loginUser.setUserStatus(UserStatus.valueOf(status));
+            return loginUser;
+        }
+    }
 }
