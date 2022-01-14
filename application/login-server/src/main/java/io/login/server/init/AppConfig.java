df636fa3c60eb389d7fb3c0e1f93fc94b8790e2b
r0p06np <rabindra.kumar.patra@walmart.com>
2022-01-14 20:43:38 +0530

    docker image creation

--- application/login-server/src/main/java/io/login/server/init/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package io.login.server.init;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:database.properties")
-public class AppConfig {
-}
